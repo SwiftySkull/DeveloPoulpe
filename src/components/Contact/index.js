aae27acfebbd8aec5972327785d73569949c8b36
SwiftySkull <aurelien.beneyton@gmail.com>
2022-03-15 16:55:51 +0100

    Contact form design

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,7 +7,6 @@ import Cookies from 'universal-cookie';
 // == Import
 import './contact.scss';
 import contact from 'src/data/Languages-files/contact';
-import { french, english } from 'src/data/languages';
 
 // == Composant
 /**
@@ -35,6 +34,35 @@ const Contact = ({
   return (
     <div id="contact">
       <h1>{contact[language].title}</h1>
+      <div className="contact-content">
+        <p>{contact[language].p1}</p>
+        <p>{contact[language].p2} <a href="mailto:aurelien.beneyton@developoulpe.fr">aurelien.beneyton@developoulpe.fr</a>.</p>
+      </div>
+      <form className="contac-form">
+        <p>{contact[language].form_p3}</p>
+        <p>{contact[language].form_p4}</p>
+        <label htmlFor="field-name" className="label-name">
+          <p>{contact[language].form.name}</p>
+          <input id="field-name" className="input-field" type="text" />
+        </label>
+        <label htmlFor="field-email" className="label-email">
+          <p>{contact[language].form.email}</p>
+          <input id="field-email" className="input-field" type="email" />
+        </label>
+        <label htmlFor="field-tel" className="label-tel">
+          <p>{contact[language].form.tel}</p>
+          <input id="field-tel" className="input-field" type="tel" />
+        </label>
+        <label htmlFor="field-subject" className="label-subject">
+          <p>{contact[language].form.subject}</p>
+          <input id="field-subject" className="input-field" type="text" />
+        </label>
+        <label htmlFor="field-message" className="label-message">
+          <p>{contact[language].form.message}</p>
+          <textarea id="field-message" className="input-field" />
+        </label>
+        <button type="submit" className="contact-button">{contact[language].form.button}</button>
+      </form>
     </div>
   );
 };
